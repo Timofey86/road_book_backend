0c1b39cd3b52d7aebab74598bb8fd519cbb54886
Timofey Mukhin <muhintv@gmail.com>
2026-09-22 12:30:55 +0200

    add dry-run option to SSL renewal script

--- deploy/renew-ssl.sh
+++ deploy/renew-ssl.sh
@@ -3,14 +3,21 @@ set -e
 
 cd /home/ec2-user/road_book_backend
 
+CERTBOT_ARGS=()
+
+if [[ "${1:-}" == "--dry-run" ]]; then
+    CERTBOT_ARGS+=(--dry-run)
+fi
+
 docker run --rm \
-  -v road_book_backend_certbot_www:/var/www/certbot \
-  -v road_book_backend_certbot_conf:/etc/letsencrypt \
-  certbot/certbot renew \
-  --webroot \
-  --webroot-path=/var/www/certbot
+    -v road_book_backend_certbot_www:/var/www/certbot \
+    -v road_book_backend_certbot_conf:/etc/letsencrypt \
+    certbot/certbot renew \
+    --webroot \
+    --webroot-path=/var/www/certbot \
+    "${CERTBOT_ARGS[@]}"
 
 docker compose \
-  --env-file .env \
-  -f docker-compose.yml \
-  exec -T nginx nginx -s reload
+    --env-file .env \
+    -f docker-compose.yml \
+    exec -T nginx nginx -s reload
